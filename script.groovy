e67ec5ebbc7beeaa929ce40e2e575a8ed50faa6a
OyedunOye <oyesinaoyedun@yahoo.com>
2026-08-21 01:53:18 +0200

    reference all workflow logic from script.groovy

---
+++ script.groovy
@@ -0,0 +1,20 @@
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'docker build -t oluwasade/demo-app:jma-2.1 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push oluwasade/demo-app:jma-2.1'
+
+    }
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+}
+
+return this
